f34febaf53d7847173f7a8e407e82b393d73dc2a
Aniket <mahendra.gupta9891@gmail.com>
2026-06-20 14:47:12 +0530

    new file

---
+++ test.py
@@ -0,0 +1,25 @@
+import random
+
+def number_guessing_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    print("मैंने 1 से 100 के बीच एक गुप्त नंबर चुना है।")
+
+    while True:
+        try:
+            guess = int(input("आपका अंदाज़ा क्या है? "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("बहुत छोटा नंबर! थोड़ा बड़ा नंबर चुनें।")
+            elif guess > secret_number:
+                print("बहुत बड़ा नंबर! थोड़ा छोटा नंबर चुनें।")
+            else:
+                print(f"बधाई हो! आपने {attempts} प्रयासों में गुप्त नंबर खोज लिया।")
+                break
+        except ValueError:
+            print("कृपया एक वैध (valid) संख्या दर्ज करें।")
+
+number_guessing_game()
+
+
